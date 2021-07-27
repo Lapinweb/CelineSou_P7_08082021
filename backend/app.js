8ed9fc51cbd95df9db2ff0937cfea8300df04b49
Céline <celinesou77@gmail.com>
2021-07-27 22:36:23 +0200

    mise en place serveur back-end

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const sequelize = require('./database');
+
+const app = express();
+
+
+
+module.exports = app;
---
+++ backend/database.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql'
+});
+
+try {
+  sequelize.authenticate();
+  console.log('Connexion à MySQL réussie.');
+}
+catch (error) {
+  console.error('Connexion à MySQL échouée :', error);
+}
+
+module.exports = sequelize;
